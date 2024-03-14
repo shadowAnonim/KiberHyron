f4cfd35e0bd88f28de37af27d2da42f05d82dd67
shadowAnonim <shadow.anonim@yandex.ru>
2024-03-14 23:44:06 +0300

    Добавьте файлы проекта.

---
+++ KiberHyron/Modules/InteractionModule.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.Interactions;
+using KiberHyron.Log;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiberHyron
+{
+    public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        public InteractionService Commands { get; set; }
+        private static Logger _logger;
+
+        public InteractionModule(ConsoleLogger logger)
+        {
+            _logger = logger;
+        }
+
+        [SlashCommand("создать_игру", "Создаёт новую игру и позволяет настроить её параметры")]
+        public async Task Create()
+        {
+            // New LogMessage created to pass desired info to the console using the existing Discord.Net LogMessage parameters
+            await _logger.Log(new LogMessage(LogSeverity.Info, "InteractionModule : Create", $"User: {Context.User.Username}, Command: создать_игру", null));
+            // Respond to the user
+            await RespondAsync("Пока не умею :(");
+        }
+
+        
+    }
+}
---
+++ KiberHyron/Program.cs
@@ -0,0 +1,88 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration.Json;
+using Discord.Commands;
+using Discord;
+using KiberHyron.Log;
+
+namespace KiberHyron
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+
+        // Program entry point
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+
+        public async Task MainAsync()
+        {
+            var config = new ConfigurationBuilder()
+            // this will be used more later on
+            .SetBasePath(AppContext.BaseDirectory)
+            // I chose using YML files for my config data as I am familiar with them
+            .AddJsonFile("config.json")
+            .Build();
+
+            using IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+            services
+            // Add the configuration to the registered services
+            .AddSingleton(config)
+            // Add the DiscordSocketClient, along with specifying the GatewayIntents and user caching
+            .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.All,
+                LogGatewayIntentWarnings = true,
+                AlwaysDownloadUsers = true,
+                LogLevel = LogSeverity.Debug
+            }))
+            // Adding console logging
+            .AddTransient<ConsoleLogger>()
+            // Used for slash commands and their registration with Discord
+            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+            // Required to subscribe to the various client events used in conjunction with Interactions
+            .AddSingleton<InteractionHandler>()
+            // Adding the prefix Command Service
+            .AddSingleton(x => new CommandService(new CommandServiceConfig
+            {
+                LogLevel = LogSeverity.Debug,
+                DefaultRunMode = Discord.Commands.RunMode.Async
+            })))
+            .Build();
+
+            await RunAsync(host);
+        }
+
+        public async Task RunAsync(IHost host)
+        {
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+
+            var commands = provider.GetRequiredService<InteractionService>();
+            _client = provider.GetRequiredService<DiscordSocketClient>();
+            var config = provider.GetRequiredService<IConfigurationRoot>();
+
+            await provider.GetRequiredService<InteractionHandler>().InitializeAsync();
+
+            // Subscribe to client log events
+            _client.Log += _ => provider.GetRequiredService<ConsoleLogger>().Log(_);
+            // Subscribe to slash command log events
+            commands.Log += _ => provider.GetRequiredService<ConsoleLogger>().Log(_);
+
+            _client.Ready += async () =>
+            {
+                await commands.RegisterCommandsGloballyAsync(true);
+            };
+
+
+            await _client.LoginAsync(Discord.TokenType.Bot, config["token"]);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
