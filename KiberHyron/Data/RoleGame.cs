00f28fea68306be10e67a53005974d20ddecc6fd
shadowAnonim <shadow.anonim@yandex.ru>
2024-03-18 23:14:29 +0300

    Классы для работы с данными

---
+++ KiberHyron/Data/RoleGame.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiberHyron.Data
+{
+    public class RoleGame
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public uint Color { get; set; }
+        public ulong Master { get; set; }
+    }
+}
---
+++ KiberHyron/Modules/GameModal.cs
@@ -0,0 +1,28 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiberHyron
+{
+    public class GameModal : IModal
+    {
+        public string Title => "Создание игры";
+        [InputLabel("Название")]
+        [ModalTextInput("name", Discord.TextInputStyle.Short, "Название игры")]
+        public string Name { get; set; }
+        [InputLabel("Описание")]
+        [ModalTextInput("description", Discord.TextInputStyle.Paragraph, "Описание игры")]
+        [RequiredInput(false)]
+        public string Description { get; set; }
+        [InputLabel("Ссылка")]
+        [ModalTextInput("link", Discord.TextInputStyle.Short, "Ссылка на Roll20")]
+        public string Link { get; set; }
+        [InputLabel("Цвет")]
+        [ModalTextInput("color", Discord.TextInputStyle.Paragraph, "HEX-значение цвета (если вы не заполните это поле, будет выбран случайный цвет)")]
+        [RequiredInput(false)]
+        public string Color { get; set; }
+    }
+}
